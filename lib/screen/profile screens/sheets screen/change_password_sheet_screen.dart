90e1f080a1853cf78d4059b0dd2bde0dcf54c297
SaifEddine8 <saifbnyslamh12@gmail.com>
2026-09-02 21:39:37 +0300

    .

--- lib/screen/profile screens/sheets screen/change_password_sheet_screen.dart
+++ lib/screen/profile screens/sheets screen/change_password_sheet_screen.dart
@@ -23,89 +23,93 @@ class _ChangePasswordSheetScreenState extends State<ChangePasswordSheetScreen> {
     TextEditingController oldPasswordController=TextEditingController();
     TextEditingController newPasswordController=TextEditingController();
     final userProvider=context.read<UpdateUserProvider>();
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Container(
-        height: MediaQuery.of(context).size.height/2,
-        child: Form(
-          key: _formkey,
-          child: Column(
-            mainAxisAlignment: .spaceEvenly,
-          children: [
-            Text('Change Password',style: ConstantStyle.titeStyle,),
-            TextFromFieldClass(controller: oldPasswordController, hint: 'Old Password', obScure: passScure,lable: 'Old Password',
-            validator: (value) {
-              if(value==null||value.isEmpty)
-              {
-                return 'Please enter your old password';
-              }
-              if(value!=userProvider.currentUser!.password)
-              {
-                return 'Incorrect old password';
-              }
-              return null;
-            },
-            preIcon: Icons.password,
-            sufIcon: InkWell(
-                    onTap: ()=>setState(() {
-                      passScure=!passScure;
-                    }),
-                    child: passScure?Icon(Icons.visibility):Icon(Icons.visibility_off)),
-      
-            ),
-      
-            
-            TextFromFieldClass(controller: newPasswordController, hint: 'New Password', obScure: passScure,lable: 'New Password',
-            validator: (value) {
-              if(Utils.checkPassword(value!))
-                      {
-                        return null;
-                      }
-                      else
-                      {return 'Incorrect password'; }
-            },
-            preIcon: Icons.password,
-            sufIcon: InkWell(
-                    onTap: ()=>setState(() {
-                      passScure=!passScure;
-                    }),
-                    child: passScure?Icon(Icons.visibility):Icon(Icons.visibility_off)),
-      
-            ),
-            SizedBox(
-              width: .infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: ConstantColors.primaryColor,
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                
-                
-                onPressed: (){
-                if(_formkey.currentState!.validate())
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(20),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Container(
+          height: MediaQuery.of(context).size.height/2,
+          child: Form(
+            key: _formkey,
+            child: Column(
+              mainAxisAlignment: .spaceEvenly,
+            children: [
+              Text('Change Password',style: ConstantStyle.titeStyle,),
+              TextFromFieldClass(controller: oldPasswordController, hint: 'Old Password', obScure: passScure,lable: 'Old Password',
+              validator: (value) {
+                if(value==null||value.isEmpty)
                 {
-                  UserModel updatedUser=UserModel(
-                    
-                    fullName: userProvider.currentUser!.fullName,
-                    email: userProvider.currentUser!.email,
-                    password: newPasswordController.text,
-                    phone: userProvider.currentUser!.phone,
-                    profileImage: userProvider.currentUser!.profileImage,
-                    location: userProvider.currentUser!.location
-                  );
-                  int userIndex = users.indexWhere((user) => user.id == userProvider.currentUser!.id);
-                  userProvider.updateUser(updatedUser);
-                  users[userIndex] = userProvider.currentUser!;
-                  Navigator.pop(context);
+                  return 'Please enter your old password';
                 }
-              }, child: Text('Change Password',style: ConstantStyle.titeStyle.copyWith(color: Colors.white),)),
-            )
-          ],
-        )
-        )
+                if(value!=userProvider.currentUser!.password)
+                {
+                  return 'Incorrect old password';
+                }
+                return null;
+              },
+              preIcon: Icons.password,
+              sufIcon: InkWell(
+                      onTap: ()=>setState(() {
+                        passScure=!passScure;
+                      }),
+                      child: passScure?Icon(Icons.visibility):Icon(Icons.visibility_off)),
+        
+              ),
+        
+              
+              TextFromFieldClass(controller: newPasswordController, hint: 'New Password', obScure: passScure,lable: 'New Password',
+              validator: (value) {
+                if(Utils.checkPassword(value!))
+                        {
+                          return null;
+                        }
+                        else
+                        {return 'Incorrect password'; }
+              },
+              preIcon: Icons.password,
+              sufIcon: InkWell(
+                      onTap: ()=>setState(() {
+                        passScure=!passScure;
+                      }),
+                      child: passScure?Icon(Icons.visibility):Icon(Icons.visibility_off)),
+        
+              ),
+              SizedBox(
+                width: .infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: ConstantColors.primaryColor,
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  
+                  
+                  onPressed: (){
+                  if(_formkey.currentState!.validate())
+                  {
+                    UserModel updatedUser=UserModel(
+                      
+                      fullName: userProvider.currentUser!.fullName,
+                      email: userProvider.currentUser!.email,
+                      password: newPasswordController.text,
+                      phone: userProvider.currentUser!.phone,
+                      profileImage: userProvider.currentUser!.profileImage,
+                      location: userProvider.currentUser!.location
+                    );
+                    int userIndex = users.indexWhere((user) => user.id == userProvider.currentUser!.id);
+                    userProvider.updateUser(updatedUser);
+                    users[userIndex] = userProvider.currentUser!;
+                    Navigator.pop(context);
+                  }
+                }, child: Text('Change Password',style: ConstantStyle.titeStyle.copyWith(color: Colors.white),)),
+              )
+            ],
+          )
+          )
+        ),
       ),
     );
   }
